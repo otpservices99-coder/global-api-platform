22ca4d28d5cbefac27ba27959ec7d52500cbc031
otpservices99-coder <otpservices99@gmail.com>
2026-09-25 13:19:30 +0100

    Update frontend API URL to Cloudflare Worker

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,6 +1,6 @@
 const API = (() => {
 
-    const BASE_URL = "http://localhost:3000";
+    const BASE_URL = "https://global-api-platform.otpservices99.workers.dev";
 
     function token() {
         return localStorage.getItem("token") || "";
